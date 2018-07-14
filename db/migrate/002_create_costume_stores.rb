28d3088555a325988fc8b8caf1ca7c853b6e5814
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-14 20:09:40 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -10,6 +10,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
+      t.timestamps
     end
   end
 end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -11,6 +11,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.datetime :opening_date
       t.datetime :closing_time
       t.string :description
+      t.timestamps
     end
   end
 end
