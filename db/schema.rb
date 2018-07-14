6fbe2ae58274ac184ed06c601a7511dda2d4edb4
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-14 20:10:04 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.boolean "still_in_business"
     t.datetime "opening_time"
     t.datetime "closing_time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "costumes", force: :cascade do |t|
@@ -27,6 +29,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.float "price"
     t.string "size"
     t.string "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "haunted_houses", force: :cascade do |t|
@@ -38,6 +42,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.datetime "opening_date"
     t.datetime "closing_time"
     t.string "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
